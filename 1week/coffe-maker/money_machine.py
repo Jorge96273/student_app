432820ad775c9e04dc395360d455c044a7385609
Jorge Ayala <jorge.ayalavalenzuela7@gmail.com>
2024-02-14 14:24:09 -0500

    coffee2

--- 1week/coffe-maker/money_machine.py
+++ 1week/coffe-maker/money_machine.py
@@ -1,8 +1,8 @@
 class MoneyMachine:
 
-    CURRENCY = "$"
+    sign_money = "$"
 
-    COIN_VALUES = {
+    coins = {
         "quarters": 0.25,
         "dimes": 0.10,
         "nickles": 0.05,
@@ -15,13 +15,13 @@ class MoneyMachine:
 
     def report(self):
         """Prints the current profit"""
-        print(f"Money: {self.CURRENCY}{self.profit}")
+        print(f"Money: {self.sign_money}{self.profit}")
 
     def process_coins(self):
         """Returns the total calculated from coins inserted."""
         print("Please insert coins.")
-        for coin in self.COIN_VALUES:
-            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        for coin in self.coins:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.coins[coin]
         return self.money_received
 
     def make_payment(self, cost):
@@ -29,7 +29,7 @@ class MoneyMachine:
         self.process_coins()
         if self.money_received >= cost:
             change = round(self.money_received - cost, 2)
-            print(f"Here is {self.CURRENCY}{change} in change.")
+            print(f"Here is {self.sign_money}{change} in change.")
             self.profit += cost
             self.money_received = 0
             return True
